6b24fbd38e49f1fc39f498287ebcfda3f606ddd4
YiyangSunn <1196168404@qq.com>
2022-05-11 22:14:29 +0000

    引入 rabbitmq，稍后继续

--- pos-delivery/src/main/java/com/micropos/delivery/model/Delivery.java
+++ pos-delivery/src/main/java/com/micropos/delivery/model/Delivery.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Delivery {
 
+    private String orderId;
+
     private String deliveryId;
 
+    private String carrier;
+
+    private List<DeliveryPhase> phases;
 }
---
+++ pos-delivery/src/main/java/com/micropos/delivery/model/DeliveryPhase.java
@@ -0,0 +1,11 @@
+package com.micropos.delivery.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliveryPhase {
+}
